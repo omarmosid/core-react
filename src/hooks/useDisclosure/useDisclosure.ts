978fae44e7944bf16ff9c3534e5f743fd42794b1
Mohammad Omar <omar.mo.sid@gmail.com>
2021-10-13 15:37:46 +0530

    Fix import for useDisclosure

--- src/hooks/useDisclosure/useDisclosure.ts
+++ src/hooks/useDisclosure/useDisclosure.ts
@@ -4,28 +4,26 @@ import { useState } from "react";
  * Custom Hook to show/hide a JSX Element or component
  * @param isOpenByDefault {boolean} - Sets initial isOpen state
  * @returns boolean, open function, close function, toggle function
- * 
+ *
  * Usage inside a component ->
- * 
+ *
  * Step 1: Import and call function
  * `const { isOpen, open, close } = useDisclosure();`
- * 
+ *
  * Step 2: Add the trigger
  * `<button onClick={open}>Open Modal</button>`
- * 
+ *
  * Step 3: Add state on the element that is hidden/visible
  * `<Modal isOpen={isOpen} close={close}>Modal Content</Modal>`
  */
- const useDisclosure = (isOpenByDefault = false) => {
-   const [isOpen, setIsOpen] = useState(isOpenByDefault);
- 
-   const open = () => setIsOpen(true);
- 
-   const close = () => setIsOpen(false);
- 
-   const toggle = () => setIsOpen(!isOpen);
- 
-   return { isOpen, open, close, toggle }
- }
- 
- export default useDisclosure;
+export const useDisclosure = (isOpenByDefault = false) => {
+  const [isOpen, setIsOpen] = useState(isOpenByDefault);
+
+  const open = () => setIsOpen(true);
+
+  const close = () => setIsOpen(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return { isOpen, open, close, toggle };
+};
